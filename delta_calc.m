16e06598be3b935902231d862c888bb007469199
eduardopoleze888 <eduardo.poleze@usp.br>
2017-08-21 17:15:57 -0300

    Cálculo da posição de parada funcionando

--- delta_calc.m
+++ delta_calc.m
@@ -2,23 +2,22 @@ clear all
 close all
 clc
 
-eps = 0.2;       % Período de atualização (período de amostragem, task rate) [s];
-
-dt = 0.001;
+dt = 0.0001;
 Tr = 0.1;
-tf = 30;
+tf = 10;
 t = (0:dt:(tf)).';
+ti1 = 3;
+ti2 = 6;
+ti3 = 10;
 nt = length(t);
-n1 = round(nt/3);
-n2 = n1;
+n1 = ti1/dt;
+n2 = (ti2-ti1)/dt;
 
-ti1 = 10;
-ti2 = 20;
-ti3 = 30;
 
-a0 = 4;
-v0 = 10;
-s0 = 0;
+
+a0 = 0.1;
+v0 = 5;
+s0 = 15;
 
 al = -(2*v0 + a0*ti1)/(ti2+ti3-ti1);
 
@@ -38,8 +37,11 @@ st_s(1) = s0;
 vt_s(1) = v0;
 at_s(1) = a0;
 
-ps1_v = [];
-ps2_v = [];
+noise1 = 0.0*randn(nt,1);
+noise2 = 0.0*randn(nt,1);
+
+ps_v = [];
+ps_f = [];
 tps = [];
 
 for i=2:nt
@@ -49,37 +51,45 @@ for i=2:nt
     st_s(i) = int_tustin(vt_s(i), vt_s(i-1), st_s(i-1), dt);
     % Scan Time
     if mod(ti, Tr) == 0
-        [delta, ts1, ts2, grau, cx]  = pol2roots(1/2*jt_s(i), at_s(i), vt_s(i));
+        [delta, ts1, ts2, grau, cx]  = pol2roots(1/2*jt_s(i), at_s(i), (vt_s(i)+ noise1(i)));
         if grau == 1
-            ps1 = p23(0, 1/2*at_s(i), vt_s(i), st_s(i), ts1);
-            ps1_v = [ps1_v ps1];
-            ps2_v = [ps2_v ps2];
+            a_ts = at_s(i) + jt_s(i)*ts1;
+            if abs(a_ts) < 0.01
+                ps_f = [ps_f 1];
+            else
+                ps_f = [ps_f 0];
+            end
+            ps = p23(0, 1/2*at_s(i), (vt_s(i) + noise1(i)), (st_s(i)+ noise2(i)), ts1);
+            ps_v = [ps_v ps];
             tps = [tps ti];
         else
             if cx == 0
-                ps1 = p23((1/6)*jt_s(i), (1/2)*at_s(i), vt_s(i), st_s(i), ts1);
-                ps1_v = [ps1_v ps1];
-                ps2 = p23((1/6)*jt_s(i), (1/2)*at_s(i), vt_s(i), st_s(i), ts2);
-                ps2_v = [ps2_v ps2];
+                if ts1 < 0
+                    if ts2 < 0
+                        ps = Inf;
+                        a_ts = at_s(i) + jt_s(i)*ts2;
+                    else
+                        ps = p23((1/6)*jt_s(i), (1/2)*at_s(i), (vt_s(i)+noise1(i)), (st_s(i)+noise2(i)), ts2);
+                        a_ts = at_s(i) + jt_s(i)*ts2;
+                    end
+                else
+                    ps = p23((1/6)*jt_s(i), (1/2)*at_s(i), (vt_s(i)+noise1(i)), (st_s(i)+noise2(i)), ts1);
+                    a_ts = at_s(i) + jt_s(i)*ts1;
+                end
+                if abs(a_ts) < 0.01
+                    ps_f = [ps_f 1];
+                else
+                    ps_f = [ps_f 0];
+                end
+                ps_v = [ps_v ps];
                 tps = [tps ti];
             end
         end
-    end  
+    end
 end
-
 figure
 hold on
-plot(tps, ps1_v, tps, ps2_v)
-title('Posiçao')
-legend('ps1', 'ps2')
-xlabel('Tempo [s]')
-ylabel('Posição [m]')
-hold off
-% sim('simu')
-% 
-figure
-hold on
-plot(t,st_s)
+plot(t,st_s + noise2)
 title('Posiçao')
 xlabel('Tempo [s]')
 ylabel('Posição [m]')
@@ -87,7 +97,7 @@ hold off
 % 
 figure
 hold on
-plot(t,vt_s)
+plot(t,vt_s + noise1)
 title('Velocidade')
 xlabel('Tempo [s]')
 ylabel('Velocidade [m/s]')
@@ -108,3 +118,15 @@ title('Jerk')
 xlabel('Tempo [s]')
 ylabel('Jerk [m/s^3]')
 hold off
+
+figure
+hold on
+plot(tps, ps_v)
+lim = axis;
+plot(tps, 0.5*lim(4)*ps_f, 'r')
+title('Parada')
+xlabel('Tempo [s]')
+ylabel('Posição [m]')
+hold off
+% sim('simu')
+% 
